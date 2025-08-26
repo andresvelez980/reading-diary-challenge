5c1c57c19efb77a3b53e82a611044497c6c4e3dc
andres velez <andrew.vm1611@gmail.com>
2025-08-25 19:45:34 -0500

    comit inicial

--- readingdiary/model.py
+++ readingdiary/model.py
@@ -1,3 +1,29 @@
 from datetime import datetime
 
 # TODO: Add code here
+
+class Note:
+    def __init__(self, text: str, page: int, date: datetime):
+        self.text: str = text
+        self.page: int = page
+        self.date: datetime = date
+
+    def __str__(self) -> str:
+        return f"{self.date} - page {self.page}: {self.text}"
+
+class Book:
+    EXCELLENT: int = 3
+    GOOD: int = 2
+    BAD: int = 1
+    UNRATED: int = -1
+
+    def __init__(self, isbn: str, title: str, author: str, pages: int, rating: int, notes: list[Note]):
+        self.isbn: str = isbn
+        self.title: str = title
+        self.author: str = author
+        self.pages: int = pages
+        self.rating: int = rating
+        self.notes: list[Note] = notes
+
+    def add_note(self, text: str, page: int, date: datetime) -> bool:
+        pass
