12db066ca4586b9ed860ac312d51a8521519473e
Andres felipe <andrew.vm1611@gmail.com>
2026-03-09 17:13:09 -0500

    creacion clase "Book"

--- readingdiary/model.py
+++ readingdiary/model.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from typing import List, Optional
 
-class note:
+class Note:
     def __init__(self, text: str, page: int, date: datetime) -> None:
         self.text = text
         self.page = page
@@ -9,3 +9,16 @@ class note:
 
     def __str__(self):
         return f"{self.date} - page {self.page}: {self.text}"
+class Book:
+    EXCELLENT: int = 3
+    GOOD: int = 2
+    BAD: int = 1
+    UNRATED: int = -1
+
+    def __init__(self, isbn: str, title: str, author: str, pages: int):
+        self.isbn = isbn
+        self.title = title
+        self.author = author
+        self.pages = pages
+        self.rating = Book.UNRATED
+        self.notes = []
